8b93b2b43b4a8630e1be1fb5be0b2022a60632e3
sgpl <sharad45@gmail.com>
2021-02-23 16:17:28 -0500

    Add code for database + create db

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #3 is a relative path, 4 is an absolute path
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    """docstring for Todo"""
+    # def __init__(self, arg):
+    #     super(Todo, self).__init__()
+    #     self.arg = arg
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False) #nullable false means user can't leave it blank
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 @app.route('/')
 
